106dd6a9421c39cef1cd13fd99666fb9607ad5a4
饭饭 <76698242@qq.com>
2017-11-30 23:45:35 +0800

    瞎J8一通乱改
    各种 Bug 修复，移动端适配等

--- index.php
+++ index.php
@@ -15,11 +15,11 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  <div id="content">
 <?php while($this->next()): ?>
 <article id="post_list">
-<section class="post">
-	<h2><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
-	<small class="meta"><?php $this->date('Y-m-d'); ?> &bull; <?php $this->category(','); ?> &bull; <a href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('没有评论', '1 条评论', '%d 条评论'); ?></a></small>
-	<p><?php $this->content('阅读更多...'); ?></p>
-	</section>
+  <section class="post">
+	<h2 class="post-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
+	<div class="post-meta"><?php $this->date('Y-m-d'); ?> &bull; <?php $this->category(','); ?> &bull; <a href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('没有评论', '1 条评论', '%d 条评论'); ?></a></div>
+	<div class="post-content"><?php $this->content('阅读更多...'); ?></div>
+  </section>
 </article>
 <?php endwhile; ?>
 </div>
