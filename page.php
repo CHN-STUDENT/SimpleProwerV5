adbb69e8dab0529954f674ea905df1a16df1e19d
CHN-STUDENT <CHN-STUDENT@outlook.com>
2017-12-01 01:04:14 +0800

    微调，文章密码输入框可能存在问题，正在研究处理。

--- page.php
+++ page.php
@@ -5,7 +5,7 @@ $this->need('header.php'); ?>
 <article id="post_list">
   <section class="post">
 	<h2 class="post-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
-	<div class="post-meta"><?php $this->date('Y-m-d'); ?> &bull; <?php $this->category(','); ?> &bull; <a href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('没有评论', '1 条评论', '%d 条评论'); ?></a></div>
+	<div class="post-meta"><?php $this->date('Y-m-d'); ?> &bull; <a href="<?php $this->permalink() ?>#comments"><?php $this->commentsNum('没有评论', '1 条评论', '%d 条评论'); ?></a></div>
 	<div class="post-content"><?php $this->content('阅读更多...'); ?></div>
   </section>
 </article>
